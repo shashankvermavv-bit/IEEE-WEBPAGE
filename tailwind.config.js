77f33f93439fc3ca8c1b330aeaa48a1b2620e688
shashank-lab-hub <266308881+shashankvermavv-bit@users.noreply.github.com>
2026-05-16 23:44:07 +0530

    all made by own

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  content: ["./**/*.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        google: ["Roboto", "sans-serif"],
+      },
+
+      colors: {
+        darkgray: "#202124", 
+        backgroundcolor:"#202124" ,
+         googleBg: "#202124",
+        primary: "#1a73e8",
+        primaryHover: "#1765cc",
+        background: "#f1f3f4",
+        surface: "#ffffff",
+        textPrimary: "#202124",
+        textSecondary: "#5f6368",
+        borderColor: "#dadce0",
+        success: "#34a853",
+        warning: "#fbbc04",
+        danger: "#ea4335"
+      }
+    }
+  }
+}
